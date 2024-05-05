143f8d568c726738506f3748dd572187cc3385d8
Penk Chen <penk.chen@qt.io>
2024-05-05 19:46:19 +0200

    Prepare for Raspberry Pi OS Bookworm updates

--- stage4.5-cutiepi/01-tweaks/01-run.sh
+++ stage4.5-cutiepi/01-tweaks/01-run.sh
@@ -12,8 +12,8 @@ install -m 644 files/dt-blob.bin 		"${ROOTFS_DIR}/boot/"
 install -m 644 files/*.dtbo 			"${ROOTFS_DIR}/boot/overlays/"
 
 install -m 755 files/cutoff 			"${ROOTFS_DIR}/usr/lib/systemd/system-shutdown/"
-install -m 644 files/cutiepi-shell.desktop	"${ROOTFS_DIR}/etc/xdg/autostart/"
 install -m 755 files/cutiepi-shell 		"${ROOTFS_DIR}/usr/local/bin/"
+install -m 644 files/cutiepi-shell.desktop     "${ROOTFS_DIR}/etc/xdg/autostart/"
 install -m 644 files/cutiepi-mcuproxy.desktop	"${ROOTFS_DIR}/etc/xdg/autostart/"
 install -m 755 files/cutiepi-mcuproxy 		"${ROOTFS_DIR}/usr/local/bin/"
 
@@ -32,25 +32,25 @@ on_chroot <<EOF
 apt-mark hold libqt5virtualkeyboard5 qtvirtualkeyboard-plugin
 EOF
 
-tar xvpf files/panel-config.tgz -C 		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+tar xvpf files/panel-config.tgz -C      "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 tar xvpf files/dconf-config.tgz -C 		"${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
 on_chroot <<EOF
 chown $FIRST_USER_NAME:$FIRST_USER_NAME -R /home/$FIRST_USER_NAME/.config/
-cp /usr/share/applications/connman-gtk.desktop /etc/xdg/autostart/
 EOF
 
 tar xvpf files/rdp-wallpaper-extra.tgz -C	"${ROOTFS_DIR}/"
 
 on_chroot <<EOF
-chown $FIRST_USER_NAME:$FIRST_USER_NAME -R /opt/cutiepi-shell/
+systemctl enable NetworkManager
 EOF
+#on_chroot <<EOF
+#chown $FIRST_USER_NAME:$FIRST_USER_NAME -R /opt/cutiepi-shell/
+#EOF
 
-rm -f "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"
 rm -f "${ROOTFS_DIR}/etc/xdg/autostart/pprompt.desktop"
-rm -f "${ROOTFS_DIR}/etc/xdg/autostart/piwiz.desktop"
 
 sed -i 's/console=serial0,115200 //'		"${ROOTFS_DIR}/boot/cmdline.txt"
 sed -i 's/quiet //'				"${ROOTFS_DIR}/boot/cmdline.txt"
 sed -i 's/splash //'				"${ROOTFS_DIR}/boot/cmdline.txt"
-sed -i 's/ignore-serial-consoles/ignore-serial-consoles video=HDMI-A-2:D video=DSI-1:800x1280@60/'	"${ROOTFS_DIR}/boot/cmdline.txt"
+#sed -i 's/ignore-serial-consoles/ignore-serial-consoles video=HDMI-A-2:D video=DSI-1:800x1280@60/'	"${ROOTFS_DIR}/boot/cmdline.txt"
